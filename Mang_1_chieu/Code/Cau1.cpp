e2c73f70ad3a9b4c43142a20b028f5f7a51e9de1
NGuyễn Đăng Khoa <164340706+nguyendangkhoa17012005@users.noreply.github.com>
2024-06-13 20:49:19 +0700

    Add files via upload

---
+++ Mang_1_chieu/Code/Cau1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main ()
+{
+	int n, k, cnt = 0;
+	int a[100];
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	cin >> k;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if(a[j] + a[i] == k && i != j)
+			cnt++;
+		}
+	}
+	cout << cnt;
+} 
+  
